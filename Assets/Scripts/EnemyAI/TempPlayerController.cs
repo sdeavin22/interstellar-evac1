490292d26dc6c4311d98ab19152db3d148a24d7c
interstellar-evac1 <HanBinMLee@gmail.com>
2024-05-07 12:26:32 -0700

    ML have enemy AI with simple statemachine: idle and attack state

---
+++ Assets/Scripts/EnemyAI/TempPlayerController.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TempPlayerController : MonoBehaviour
+{
+    public float speed = 5.0f;
+
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        transform.Translate(movement * speed * Time.deltaTime);
+    }
+}
